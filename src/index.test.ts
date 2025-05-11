4c15cfaaae56cbf632a0af88389bae6697cc01d2
Dimitri Lavrenük <20122620+dlavrenuek@users.noreply.github.com>
2025-05-11 19:30:35 +0200

    adjust integration tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,3 @@
-import process from "node:process";
 import cp from "node:child_process";
 import path from "node:path";
 import { promisify } from "node:util";
@@ -7,15 +6,9 @@ const exec = promisify(cp.exec);
 
 describe("index.ts", () => {
   test("The action is executed without an error", async () => {
-    const env = {
-      ...process.env,
-      INPUT_FROM: "",
-      INPUT_TO: "HEAD",
-    };
-
     const ip = path.join(__dirname, "index.ts");
     const { stdout, stderr } = await exec(`pnpm exec ts-node ${ip}`, {
-      env,
+      env: process.env,
     });
 
     console.log("stdout", stdout);
