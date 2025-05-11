95fc1a0c58b38a6dece284bfbd05113d6d85aea6
Dimitri Lavrenük <20122620+dlavrenuek@users.noreply.github.com>
2025-05-11 20:12:41 +0200

    try something

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
 };
